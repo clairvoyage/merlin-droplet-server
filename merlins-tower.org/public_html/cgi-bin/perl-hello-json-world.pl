6e388f4744bf94fdf9e3c47443e8f439d1296a99
clairvoyage <merlinho101@gmail.com>
2025-08-17 20:05:42 -0700

    added my name to perl hello json world pl

--- merlins-tower.org/public_html/cgi-bin/perl-hello-json-world.pl
+++ merlins-tower.org/public_html/cgi-bin/perl-hello-json-world.pl
@@ -10,7 +10,7 @@ print "Content-type: application/json\n\n";
 $date = localtime();
 $address = $ENV{REMOTE_ADDR};
 
-my %message = ('title' => 'Hello, Perl!', 'heading' => 'Hello, Perl!', 'message' => 'This page was generated with the Perl programming language', 'time' => $date, 'IP' => $address);
+my %message = ('title' => 'Hello, Perl, from Merlin!', 'heading' => 'Hello, Perl, from Merlin!', 'message' => 'This page was generated with the Perl programming language', 'time' => $date, 'IP' => $address);
 
 my $json = encode_json \%message;
 print "$json\n";
