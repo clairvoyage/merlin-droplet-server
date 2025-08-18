35981bbf0ca7a7d27f9b541723b5f8b4dc49840b
clairvoyage <merlinho101@gmail.com>
2025-08-17 20:03:44 -0700

    fixed error log

--- merlins-tower.org/public_html/cgi-bin/perl-hello-json-world.pl
+++ merlins-tower.org/public_html/cgi-bin/perl-hello-json-world.pl
@@ -1,5 +1,6 @@
-use CGI::Carp qw(fatalsToBrowser warningsToBrowser);
 #!/usr/bin/perl
+use CGI::Carp qw(fatalsToBrowser warningsToBrowser);
+
 # In Perl, you must first install the JSON package from CPAN (the Perl equivalent to npm)
 use JSON;
 
