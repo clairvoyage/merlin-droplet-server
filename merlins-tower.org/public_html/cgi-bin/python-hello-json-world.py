df1caf97f687b747b1bbcf889ee677b39e7b520c
clairvoyage <merlinho101@gmail.com>
2025-08-19 12:33:52 -0700

    added time and ip address to python json file

--- merlins-tower.org/public_html/cgi-bin/python-hello-json-world.py
+++ merlins-tower.org/public_html/cgi-bin/python-hello-json-world.py
@@ -2,12 +2,20 @@
 # Importing the JSON module and cgi module
 import json
 import cgi
+import datetime
+import socket
+
+# Get current date and time
+current_time = datetime.datetime.now()
+# Get IP address
+host = socket.gethostname()
+IPAddress = socket.gethostbyname(host + ".local")
 
 # Send HTTP header saying content is JSON
 print("Cache-Control: no-cache\n")
 print("Content-type: application/json\n\n")
 
-data = {"title": "Hello Python!", "heading": "Hello Python!"}
+data = {"title": "Hello Python!", "heading": "Hello Python!", "IP-address": IPAddress, "date-and-time": current_time}
 json_string = json.dumps(data, indent=4)
 
 # Write to JSON file.. feels weird
