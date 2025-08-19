65ee87a3fe13e28e2d3d0fff96650395bca80f8c
clairvoyage <merlinho101@gmail.com>
2025-08-18 17:54:28 -0700

    addded first python file

---
+++ merlins-tower.org/public_html/cgi-bin/python-hello-html-world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Importing the 'cgi' module
+import cgi
+
+# Send HTTP header saying content is HTML
+print("Content-type: text/html\n\n")
+
+# Hello world in an HTML document
+print("<html><body>")
+print("<h1>Hello world, from our team!</h1>")
+print("</body></html>")
