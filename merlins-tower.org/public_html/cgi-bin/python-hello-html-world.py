23a6dc7a639a0921e93c068dc8ae3b4848ab4948
clairvoyage <merlinho101@gmail.com>
2025-08-19 00:15:37 -0700

    missed an angle bracket in my hello world python

--- merlins-tower.org/public_html/cgi-bin/python-hello-html-world.py
+++ merlins-tower.org/public_html/cgi-bin/python-hello-html-world.py
@@ -15,7 +15,7 @@ print("Content-type: text/html\n\n")
 # Hello world in an HTML document
 print("<html><body>")
 print("<h1>Hello world, from Merlin and Jinwoong!</h1>")
-print("<p>Current time: " + current_time + "</p")
+print("<p>Current time: " + current_time + "</p>")
 print("<p>IP Address: " + ' '.join(IPAdrress) + "</p>")
 
 print("</body></html>")
