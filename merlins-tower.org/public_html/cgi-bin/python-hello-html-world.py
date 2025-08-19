bd485466416330c4bed6ee8c21fdaf8ef6d9eeb1
clairvoyage <merlinho101@gmail.com>
2025-08-19 00:57:25 -0700

    please. please. print the ip address

--- merlins-tower.org/public_html/cgi-bin/python-hello-html-world.py
+++ merlins-tower.org/public_html/cgi-bin/python-hello-html-world.py
@@ -7,7 +7,7 @@ import socket
 current_time = datetime.datetime.now()
 # Get IP address
 host = socket.gethostname()
-IPAdrress = socket.gethostbyaddr(host)
+IPAdrress = socket.gethostbyname(host)
 
 # Send HTTP header saying content is HTML
 print("Content-type: text/html\n\n")
@@ -16,6 +16,5 @@ print("Content-type: text/html\n\n")
 print("<html><body>")
 print("<h1>Hello world, from Merlin and Jinwoong!</h1>")
 print("<h3>Current time: {0}</h3>".format(current_time))
-print("<h3>IP Address: {0}</h3>".format(' '.join(IPAdrress)))
-
+print("<h3>IP Address: {0}</h3>".format(IPAdrress))
 print("</body></html>")
