90f9715202b3b145374ef0b6502dcc0fc444b687
clairvoyage <merlinho101@gmail.com>
2025-08-19 20:22:41 -0700

    added basic python general request echo

--- merlins-tower.org/public_html/cgi-bin/python-general-request-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-general-request-echo.py
@@ -1,3 +1,11 @@
-x = 1
-print(type(x))
-print(type(str(type(type(x)))))
+#!/usr/bin/python3
+# Importing the cgi module
+import cgi, sys, cgitb, os
+cgitb.enable()
+
+# Send HTTP header saying content is HTML
+print("Content-type: text/html\n\n")
+
+print("Protocol: {0}<br>".format(os.environ.get("SERVER_PROTOCOL")))
+print("Method: {0}<br>".format(os.environ.get("REQUEST_METHOD")))
+
