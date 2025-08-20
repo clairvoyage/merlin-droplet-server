e6473eda367f49fe9f73de61e9af66cfdeffe139
clairvoyage <merlinho101@gmail.com>
2025-08-19 18:52:08 -0700

    made python env print environment name + value instead of just name

--- merlins-tower.org/public_html/cgi-bin/python-env.py
+++ merlins-tower.org/public_html/cgi-bin/python-env.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # Importing the cgi module
-import cgi
-import cgitb
+import cgi, sys, cgitb, os
 cgitb.enable()
 
 # Send HTTP header saying content is HTML
 print("Content-type: text/html\n\n")
-cgi.print_environ_usage();
+
+# Code from https://www.go4expert.com/articles/print-cgi-environment-vairables-python-t2025/
+for name, value in os.environ.items():
+    print("%s\t= %s <br/>" % (name, value))
---
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Importing the JSON module and cgi module
+import json
+import cgi
+import socket
+
