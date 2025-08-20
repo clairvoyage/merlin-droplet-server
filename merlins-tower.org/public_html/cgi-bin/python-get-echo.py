4822a6f77e6d708d808fc1a6fa3b931b0f02b387
clairvoyage <merlinho101@gmail.com>
2025-08-19 19:16:34 -0700

    added python get echo.. a fix

--- merlins-tower.org/public_html/cgi-bin/python-get-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-query_string = os.environ.get("QUERY_STRING");
+query_string = os.environ.get("QUERY_STRING")
 params = query_string.split('&')
 for param in params:
     print(params)
