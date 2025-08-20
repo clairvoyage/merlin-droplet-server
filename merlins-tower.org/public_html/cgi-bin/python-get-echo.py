8c6a86fabb2f94250d65278dcad23660ecac9037
Merlin <merlin@example.com>
2025-08-20 02:29:10 +0000

    better formatting for python get echo

--- merlins-tower.org/public_html/cgi-bin/python-get-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -5,9 +5,15 @@ cgitb.enable()
 print("Content-type: text/html\n\n")
 
 query_string = os.environ.get("QUERY_STRING")
+print("Query string: {0}".format(query_string))
 params = query_string.split('&')
+
 for param in params:
-    print(params)
+    param = param.split('=')
+    name = param[0]
+    value = param[1]
+    print("{0} = {1}".format(name, value))
+
 
 
 
