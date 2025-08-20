5154b91f2cbdda600d72ebf879a57a69e0aff83b
clairvoyage <merlinho101@gmail.com>
2025-08-19 18:07:08 -0700

    made datetime json serializable in hello world json python

--- merlins-tower.org/public_html/cgi-bin/python-hello-json-world.py
+++ merlins-tower.org/public_html/cgi-bin/python-hello-json-world.py
@@ -8,16 +8,16 @@ import socket
 # Get current date and time
 current_time = datetime.datetime.now()
 # Get IP address
-host = socket.gethostname()
-IPAddress = socket.gethostbyname(host + ".local")
+# host = socket.gethostname()
+# IPAddress = socket.gethostbyname(host + ".local")
 
 # Send HTTP header saying content is JSON
 print("Cache-Control: no-cache\n")
 print("Content-type: application/json\n\n")
 
-data = {"title": "Hello Python!", "heading": "Hello Python!", "IP-address": IPAddress, "date-and-time": current_time}
-json_string = json.dumps(data, indent=4)
+data = {"title": "Hello Python!", "heading": "Hello Python!", "date-time": current_time.strftime("%Y-%m-%d %H:%M:%S")}
+hello_python_json = json.dumps(data, indent=4)
 
 # Write to JSON file
 with open ("../json/python-hello-json-world.json", "w") as f:
-    f.write(json_string)
+    f.write(hello_python_json)
