6347a66d006546119714774a75f3220bcf2b7b64
clairvoyage <merlinho101@gmail.com>
2025-08-19 20:28:16 -0700

    added printing body to python general request echo

--- merlins-tower.org/public_html/cgi-bin/python-general-request-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-general-request-echo.py
@@ -5,7 +5,26 @@ cgitb.enable()
 
 # Send HTTP header saying content is HTML
 print("Content-type: text/html\n\n")
+protocol = os.environ.get("SERVER_PROTOCOL")
+method = os.environ.get("REQUEST_METHOD")
+print("Protocol: {0}<br>".format(protocol))
+print("Method: {0}<br>".format(method))
 
-print("Protocol: {0}<br>".format(os.environ.get("SERVER_PROTOCOL")))
-print("Method: {0}<br>".format(os.environ.get("REQUEST_METHOD")))
+params = ""
 
+if protocol == "GET":
+    query_string = os.environ.get("QUERY_STRING")
+    params = query_string.split('&')
+
+if protocol == "POST":
+    post_body = sys.stdin.read()
+    params = post_body.split('&')
+
+for param in params:
+    param = param.split('=')
+    name = param[0]
+    if(len(param)) > 1:
+        value = param[1]
+        print("{0} = {1}<br>".format(name, value))
+        continue
+    print("{0}<br>".format(name))
