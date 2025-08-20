1f362a8574f2ffaadc6d813cd339c8f6778fe047
clairvoyage <merlinho101@gmail.com>
2025-08-19 19:19:41 -0700

    query string pls work

--- merlins-tower.org/public_html/cgi-bin/python-get-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -4,9 +4,10 @@ import os
 print("Content-type: text/html\n\n")
 
 query_string = os.environ.get("QUERY_STRING")
-params = query_string.split('&')
-for param in params:
-    print(params)
+print(query_string)
+# params = query_string.split('&')
+# for param in params:
+#     print(params)
 
 
 
