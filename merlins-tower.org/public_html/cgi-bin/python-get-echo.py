994e180a8a796af196443ff1c82df0e6b90f1398
Merlin <merlin@example.com>
2025-08-20 02:22:03 +0000

    query string

--- merlins-tower.org/public_html/cgi-bin/python-get-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import os
+import cgi, cgitb, os, sys
+cgitb.enable()
 
 print("Content-type: text/html\n\n")
 
 query_string = os.environ.get("QUERY_STRING")
-print(query_string)
+print(str(query_string))
 # params = query_string.split('&')
 # for param in params:
 #     print(params)
