b0acdff0e8cb7169b690075fd82599d9fb7036bd
Merlin <merlin@example.com>
2025-08-20 02:23:46 +0000

    splitting query

--- merlins-tower.org/public_html/cgi-bin/python-get-echo.py
+++ merlins-tower.org/public_html/cgi-bin/python-get-echo.py
@@ -5,10 +5,9 @@ cgitb.enable()
 print("Content-type: text/html\n\n")
 
 query_string = os.environ.get("QUERY_STRING")
-print(str(query_string))
-# params = query_string.split('&')
-# for param in params:
-#     print(params)
+params = query_string.split('&')
+for param in params:
+    print(params)
 
 
 
