4c0280d682f180264a7161964234de2ebeca3095
clairvoyage <merlinho101@gmail.com>
2025-08-28 00:25:46 -0700

    added teeny bit to collector script

---
+++ merlins-tower.org/public_html/scripts/collector.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let staticData = {};
+    staticData["userAgent"] = navigator.userAgent;
+    
+    console.log(staticData);
+});
