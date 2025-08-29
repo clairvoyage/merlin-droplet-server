4da96a7e40afecb07ce1e4f198f9d66d01c3bdd7
clairvoyage <merlinho101@gmail.com>
2025-08-28 17:55:06 -0700

    trying out get request with data

--- merlins-tower.org/public_html/scripts/collector.js
+++ merlins-tower.org/public_html/scripts/collector.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let staticData = {};
     staticData["userAgent"] = navigator.userAgent;
+    staticData["userLanguage"] = navigator.language;
+    staticData["acceptsCookies"] = navigator.cookieEnabled;
     
+    document.cookie = "js_enabled=true; path=/"; 
+    staticData["allowsJS"] = true;
+
     console.log(staticData);
+    sendData(staticData);
+
+    
 });
+
+async function sendData(staticData) {
+    await fetch("../cgi-bin/php-general-echo.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(staticData)
+    });
+}
